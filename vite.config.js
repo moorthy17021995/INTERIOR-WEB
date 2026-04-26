a6cf7b5302f8e6091fb7cb66ea9bd0222c5b534b
moorthy17021995 <moorthy17021995@gmail.com>
2026-04-26 19:17:07 +0530

    Final check: basename and router fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
+  // Ensure the slashes are exactly like this:
   base: command === 'build' ? '/INTERIOR-WEB/' : '/',
 }))
